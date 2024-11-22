2ce1c280814acf04244cf6d31cbe891407212ad5
unknown <udarasenarath875@gmail.com>
2024-11-22 19:48:33 +0530

    Added Home Page with Hero and Header Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './Layout';
+import Home from './pages/Home/Home';
 
 const App = () => {
   return (
     <Router>
       <Layout>
         <Routes>
-          
+        <Route path="/" element={<Home />} />
         </Routes>
       </Layout>
     </Router>
