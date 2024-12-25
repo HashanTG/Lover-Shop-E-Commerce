dcde738793c768fd6018fe28ec23b694c60d0ef3
Udara Shanuka Senarath <123650100+udaaraSH23@users.noreply.github.com>
2024-12-25 15:12:56 +0530

    Styling for Pages

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,19 +1,23 @@
 import React, { useState } from "react";
 import "./header.css";
-import CartIndicatorCard from '../../components/shared/CartIndicator/CartIndicatorCard'
-import searchIcon from "../../assets/Header/search.svg";
-import userCircle from "../../assets/Header/user-circle.svg";
+import CartIndicatorCard from "../../components/shared/CartIndicator/CartIndicatorCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faUserCircle } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   const [shopDropdown, setShopDropdown] = useState(false);
   const [productDropdown, setProductDropdown] = useState(false);
+  const [profileMenuVisible, setProfileMenuVisible] = useState(false);
+
+  const toggleProfileMenu = () => {
+    setProfileMenuVisible(!profileMenuVisible);
+  };
 
   return (
     <header className="header">
-      <div className="logo">
+      <div className="nav-logo">
         <span>ROSA LOVER</span>
         <img src="https://via.placeholder.com/150" alt="Rosa Lover" />
-
       </div>
       <nav className="nav">
         <ul>
@@ -33,26 +37,42 @@ const Header = () => {
             )}
           </li>
           <li
-               className="dropdown"
-               onMouseEnter={() => setProductDropdown(true)}
-               onMouseLeave={() => setProductDropdown(false)}
-              >
-      <a href="#product">Product <span>&#9662;</span></a>
-        {productDropdown && (
-        <ul className="dropdown-menu">
-      <li><a href="/products/teddy">Teddy</a></li>
-      <li><a href="/products/jewelry">Jewelry</a></li>
-      <li><a href="/products/flowers">Flowers</a></li>
-             </ul>
-           )}
-      </li>
-
+            className="dropdown"
+            onMouseEnter={() => setProductDropdown(true)}
+            onMouseLeave={() => setProductDropdown(false)}
+          >
+            <a href="#product">Product <span>&#9662;</span></a>
+            {productDropdown && (
+              <ul className="dropdown-menu">
+                <li><a href="/products/teddy">Teddy</a></li>
+                <li><a href="/products/jewelry">Jewelry</a></li>
+                <li><a href="/products/flowers">Flowers</a></li>
+              </ul>
+            )}
+          </li>
           <li><a href="#contact">Contact Us</a></li>
         </ul>
       </nav>
       <div className="icons">
-        <a href="#search" className="icon"><img src={searchIcon} alt="Search" /></a>
-        <a href="#account" className="icon"> <img src={userCircle} alt="UserCircle" /></a>
+        <a href="#search" className="icon">
+          <FontAwesomeIcon icon={faSearch} />
+        </a>
+        <div className="icon user-icon" onClick={toggleProfileMenu}>
+          <FontAwesomeIcon icon={faUserCircle} />
+          {profileMenuVisible && (
+            <div className="profile-menu">
+              <button className="profile-menu-btn">Sign In</button>
+              <button className="profile-menu-btn">Register</button>
+              <hr />
+              <ul>
+                <li><a href="#my-orders">My Orders</a></li>
+                <li><a href="#wishlist">Wish List</a></li>
+                <li><a href="#settings">Settings</a></li>
+                <li><a href="#help-center">Help Center</a></li>
+              </ul>
+            </div>
+          )}
+        </div>
         <CartIndicatorCard />
       </div>
     </header>
