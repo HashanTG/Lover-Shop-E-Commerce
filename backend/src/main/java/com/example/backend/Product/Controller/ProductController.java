e122b76517dcc0981351c75872c5986702823050
Hashan Tharanga Gamage <109969514+HashanTG@users.noreply.github.com>
2025-01-11 15:19:52 +0530

    change in accountsection

--- backend/src/main/java/com/example/backend/Product/Controller/ProductController.java
+++ backend/src/main/java/com/example/backend/Product/Controller/ProductController.java
@@ -1,18 +1,21 @@
 package com.example.backend.Product.Controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.backend.Product.Model.Product;
 import com.example.backend.Product.Service.ProductService;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
@@ -35,7 +38,7 @@ public class ProductController {
     }
 
     // Add a new product
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('USER')")
     @PostMapping
     public Product addProduct(@RequestBody Product product) {
         return productService.addProduct(product);
