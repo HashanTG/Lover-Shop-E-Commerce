d52422a852e610930f28adecf20ef3a372ecad97
udaaraSH23 <udarasenarath875@gmail.com>
2025-02-05 17:46:42 +0530

    Home page improvemnt with email sending

--- frontend/src/pages/Home/NewArivals-Section/NewArrivals.jsx
+++ frontend/src/pages/Home/NewArivals-Section/NewArrivals.jsx
@@ -3,16 +3,19 @@ import "./NewArrivals.css";
 import { getNewArrivals } from "../../../api/productService";
 import { useState, useEffect } from "react";
 import ProductCard from "../../../components/ProductCard";
+import Spinner from "../../../components/Spinner/Spinner";
 
 function NewArrivals() {
   const [products, setProducts] = useState([]);
 
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await getNewArrivals();
         setProducts(response);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching new arrivals:", error);
       }
@@ -22,13 +25,21 @@ function NewArrivals() {
 
   return (
     <div className="new-arrivals">
-      <h1>New Arrivals</h1>
+    <h1>New Arrivals</h1>
+  
+    {loading ? (
+      <div className="spinner-container">
+        <Spinner size="48px" color="#ff12dc" />
+      </div>
+    ) : (
       <div className="product-grid">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
-    </div>
+    )}
+  </div>
+  
   );
 }
 
